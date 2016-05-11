0b47dec3b1e042e04680ae08109cad4aac848781
Anirban Biswas <anirban@cs.washington.edu>
2016-05-10 18:11:00 -0700

    done, no tuning

--- sgd.py
+++ sgd.py
@@ -4,11 +4,16 @@ import numpy as np
 
 # TODO: Calculate logistic
 def logistic(x):
-    pass
+    return (1.0 / (1 + np.exp(-x)))
 
 # TODO: Calculate accuracy of predictions on data
 def accuracy(data, predictions):
-    pass
+    total = len(predictions)
+    correct = 0
+    for i in range(0, len(predictions)):
+        if ((predictions[i] <= 0.5 and data[i]['label'] == 0) or (predictions[i] > 0.5 and data[i]['label'] == 1)):
+            correct += 1
+    return (0.0 + correct) / total
 
 class model:
     def __init__(self, structure):
@@ -20,27 +25,50 @@ class model:
             
     # TODO: Calculate prediction based on model
     def predict(self, point):
-        pass
+        a = self.feedforward(point)
+        return a[-1]
 
     # TODO: Update model using learning rate and L2 regularization
     def update(self, a, delta, eta, lam):
-        pass
+        for i in range(0, len(self.weights)):
+            self.weights[i] = self.weights[i] - eta * (lam * self.weights[i] - np.transpose(a[i] * delta[i].item(0)))
+            self.bias[i] = self.bias[i] - eta * np.transpose(-delta[i].item(0))
 
     # TODO: Perform the forward step of backpropagation
     def feedforward(self, point):
-        pass
+        a = []
+        a.append(point['features'])
+        for i in range(0, len(self.weights)):
+            postsynaptic = logistic(np.dot(a[i], self.weights[i]) + self.bias[i])
+            a.append(postsynaptic)
+        return a
     
     # TODO: Backpropagate errors
     def backpropagate(self, a, label):
-        pass
+        result_delta = []
+        L = len(a) - 1
+        delta = label - a[-1]
+        result_delta.insert(0, delta)
+        while L > 1:
+            delta = np.dot(delta, np.multiply(a[L - 1], 1 - a[L - 1]))
+            delta = np.multiply(self.weights[L - 1], np.transpose(delta))
+            L -= 1
+            result_delta.insert(0, delta)
+        return result_delta
 
     # TODO: Train your model
     def train(self, data, epochs, rate, lam):
-        pass
+        for i in range(0, epochs * len(data)):
+            point = random.choice(data)
+            a = self.feedforward(point)
+            delta = self.backpropagate(a, point['label'])
+            self.update(a, delta, rate, lam)
 
 def logistic_regression(data, lam=0.00001):
+    epochs = 100
+    eta = 0.05
     m = model([data[0]["features"].shape[1], 1])
-    m.train(data, 100, 0.05, lam)
+    m.train(data, epochs, eta, lam)
     return m
     
 def neural_net(data, lam=0.00001):
